3420e00f4f741e6a5b6b6c46df7c2df39027502f
yshtripathi <yashtripathi0905@gmail.com>
2025-05-03 16:22:53 +0530

    Initial commit

---
+++ src/main/java/com/chat/App/controller/ChatController.java
@@ -0,0 +1,23 @@
+package com.chat.App.controller;
+
+import com.chat.App.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChatController {
+
+    // /app/sendMessage
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/messages")
+    public ChatMessage sendMessage(ChatMessage message){
+        return message;
+    }
+
+    @GetMapping("chat")
+    public String chat(){
+        return "chat";
+    }
+}
---
+++ src/main/java/com/chat/App/model/ChatMessage.java
@@ -0,0 +1,13 @@
+package com.chat.App.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ChatMessage {
+    private Long id;
+    private String sender;
+    private String content;
+
+}
